797345288ff9f4b82066245e0576c8e888498391
“SahajShakya” <saz.shakya@gmail.com>
2025-01-07 14:19:11 +0545

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import Register from './Register.tsx'
+import React from "react";
+import Register from "./Register.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './Login.tsx';
-import Dashboard from './Dashboard.tsx';
+import Login from "./Login.tsx";
+import Dashboard from "./Dashboard.tsx";
 
+const PageNotFound: React.FC = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
 
-
-  const App: React.FC = () => {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>
-  )
-}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
