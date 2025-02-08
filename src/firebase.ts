7295b5e7b020474a38df4229bddd6d566e3bec0c
ssmarika <smarikashrestha02@gmail.com>
2025-02-08 10:11:43 +0545

    LoginRegister FIrebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBTWlnWg0majYWR1ReqN0LwZ0HeUyOWzds',
+  authDomain: 'register-login-ec971.firebaseapp.com',
+  projectId: 'register-login-ec971',
+  storageBucket: 'register-login-ec971.firebasestorage.app',
+  messagingSenderId: '125543200473',
+  appId: '1:125543200473:web:a40cf533ea94da53c3b0c8',
+  measurementId: 'G-SK6KR0B7KM',
+};
 
-// const firebaseConfig: FirebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_STORAGE_BUCKET",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID",
-//   measurementId: "YOUR_MES_ID",
-// };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
