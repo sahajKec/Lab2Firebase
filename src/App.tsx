ff4fb50e3bac12cd1bbc3c45ec6e31e4a720b4d7
utsarga01 <regmiutsarga7@gmail.com>
2025-02-18 17:19:03 +0545

    changed router type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Register from './Register.tsx'
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Login.tsx';
 import Dashboard from './Dashboard.tsx';
 
@@ -8,13 +8,13 @@ import Dashboard from './Dashboard.tsx';
 
   const App: React.FC = () => {
   return (
-    <Router>
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  </Router>
+  </BrowserRouter>
   )
 }
 
