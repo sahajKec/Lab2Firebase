b72fe3887c7a87769c6302041a86f624b47c0d5f
Normi333 <shisirthapa146@gmail.com>
2025-02-18 12:57:55 +0545

    kan077bct077

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA8GnhcsjMArD_lEpcqAqxjVOzp2i-Tw18",
-  authDomain: "lab2firebasebct2077.firebaseapp.com",
-  projectId: "lab2firebasebct2077",
-  storageBucket: "lab2firebasebct2077.firebasestorage.app",
-  messagingSenderId: "475061626743",
-  appId: "1:475061626743:web:80cb68c3dc20cfc1652372",
-  measurementId: "G-9HF2MT3GXJ",
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
