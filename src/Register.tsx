c481c6c6efe1f33910da707e65e4594c15601db3
RakeshPandey0 <rp901522@gmail.com>
2025-02-18 09:46:07 +0545

    Lab 2 completed

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,5 +1,5 @@
-//Make students do this
 import React, { useState, ChangeEvent, useEffect } from "react";
+import { Alert, AlertTitle, Box } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   createUserWithEmailAndPassword,
@@ -8,23 +8,22 @@ import {
 } from "firebase/auth";
 import { auth } from "./firebase";
 import { useNavigate } from "react-router-dom";
+import { getFirestore, doc, setDoc, serverTimestamp } from "firebase/firestore";
+import "./Register.css";
 
-interface RegisterProps {}
-
-const Register: React.FC<RegisterProps> = () => {
+const Register: React.FC = () => {
+  const db = getFirestore();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const checkToken = () => {
-      const userToken = localStorage.getItem("token");
-      if (userToken) {
-        navigate("/dashboard");
-      }
-    };
-    checkToken();
-  }, []);
+    const userToken = localStorage.getItem("token");
+    if (userToken) {
+      navigate("/dashboard");
+    }
+  }, [navigate]);
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -34,69 +33,92 @@ const Register: React.FC<RegisterProps> = () => {
     setPassword(e.target.value);
   };
 
-  const handleLogin = async () => {
-    // console.log('Logging in with:', { email, password });
+  const handleRegister = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password).then(
-        (userData) => {
-          if (userData.user.uid) {
-            sendEmailVerification(userData.user);
-            alert("Registration Success");
-            signOut(auth);
-            navigate("/");
-          }
-        }
+      const userData = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
       );
+      const user = userData.user;
+
+      await sendEmailVerification(user);
+
+      // Set user data in Firestore
+      await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        name: "user", // Explicitly set username to 'user'
+        email,
+        lastlogin: serverTimestamp(),
+        isVerified: user.emailVerified,
+        isActive: false,
+        createdAt: serverTimestamp(),
+        updatedAt: serverTimestamp(),
+      });
+
+      setSuccess(true);
+      signOut(auth);
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
     } catch (error: any) {
-      console.log("Error msg: ", error.message);
+      console.error("Error:", error.message);
       alert(error.message);
     }
   };
 
   return (
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="card p-4">
-        <h3 className="card-title text-center mb-4">Register</h3>
-        <form>
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </div>
-          <div className="text-center">
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleLogin}
-            >
-              Register
-            </button>
-          </div>
-        </form>
-        <h3 className="d-flex justify-content-center align-items-center">
-          <a href="/">Login</a>
-        </h3>
-      </div>
-    </div>
+    <>
+      {success && (
+        <Alert severity="success" className="alert">
+          <AlertTitle>Success</AlertTitle>
+          Registration successful. Check your email for confirmation.
+        </Alert>
+      )}
+      <Box className="register-container">
+        <div className="card">
+          <h3 className="card-title">Register</h3>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <div className="text-center">
+              <button
+                type="button"
+                className="btn-primary"
+                onClick={handleRegister}
+              >
+                Register
+              </button>
+            </div>
+          </form>
+          <p className="text-center">
+            Already have an account?<a href="/"> Login</a>
+          </p>
+        </div>
+      </Box>
+    </>
   );
 };
 
