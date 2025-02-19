87497b34e3af21eda30f31cae6b7e0f6ce5ef4ad
alsrynr <alisha.rauniyarals@gmail.com>
2025-02-19 15:10:44 +0545

    kan077bct010

--- src/Register.tsx
+++ src/Register.tsx
@@ -18,10 +18,6 @@ const Register: React.FC<RegisterProps> = () => {
        if (userToken){
         navigate("/dashboard")
        }
-       else {
-           console.log("User is not valid")
-           navigate("/")
-       }
     }
     checkToken()
    }, [])
