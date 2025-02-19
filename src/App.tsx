c3ebdbe12c6c108386bb79a5cf218fffaa42fda1
Prashant Acharya <acharyaprashant16@gmail.com>
2025-02-19 09:00:44 +0545

    Route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Register from './Register.tsx';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Register.tsx';
 import Login from './Login.tsx';
 import Dashboard from './Dashboard.tsx';
 
